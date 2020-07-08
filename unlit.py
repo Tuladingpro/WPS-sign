e317ae3894c356ca47d3f3b75a929bc824d72bfe
Tulading <nkuzhangshn@gmail.com>
2020-07-08 10:48:21 +0800

    0708test

---
+++ unlit.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+
+#Installing collected packages: wrapt, lazy-object-proxy, astroid, isort, mccabe, pylint
+#    Running setup.py install for wrapt ... done
+#  WARNING: The script isort is installed in '/home/admin/.local/bin' which is not on PATH.
+#  Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
+# WARNING: The scripts epylint, pylint, pyreverse and symilar are installed in '/home/admin/.local/bin' which is not on PATH.
+#  Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
+#Successfully installed astroid-2.4.2 isort-4.3.21 lazy-object-proxy-1.4.3 mccabe-0.6.1 pylint-2.5.3 wrapt-1.12.1
